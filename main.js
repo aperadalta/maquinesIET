d950d0b30a2e9ee42bc97516b2e3ab6d8f509cd6
Elheyr <adria.peradalta@gmail.com>
2021-05-12 20:40:01 +0200

    totally new lol

--- main.js
+++ main.js
@@ -1,54 +1,261 @@
-class maquines{
-    constructor(maquina, procediment, seguretat, manteniment){
-        this.maquina = maquina;
-        this.procediment = procediment;
-        this.seguretat = seguretat;
-        this.manteniment = manteniment;
+var assignatures = [
+    {
+        nom: "Assecadora",
+        pacs: [
+            {
+                titol: "Procediment assecadora",
+                link: "https://drive.google.com/file/d/1LK4q3-kb5baMk_tKh8BGfOshKfrc8APR/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Cabina de rentat",
+        pacs: [
+            {
+                titol: "Cabina de lavado",
+                link: "https://drive.google.com/file/d/1l8iHwNPYawFdxg8V6yn2M6PMth4jRqRd/view?usp=sharing"
+            },
+            {
+                titol: "Emulsionar pantallas Serigrafía",
+                link: "https://drive.google.com/file/d/1y25ErUAXhLN2gDDS_LhtS3IURlBOLN26/view?usp=sharing"
+            },
+            {
+                titol: "Recuperació de pantalles de Serigrafía",
+                link: "https://drive.google.com/file/d/1oCtDW8feOr5TAgatYcO5KD85QYfN-JmC/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Fitxes de seguretat",
+        pacs: [
+            {
+                titol: "Fitxes de seguretat",
+                link: "https://drive.google.com/file/d/1RiCQe60yNFRrPZRZVkZVfQvsio_assEo/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "Cosidora",
+        pacs: [
+            {
+                titol: "Hohner Economy",
+                link: "https://drive.google.com/file/d/1P-4s2oLi8T3imRAQRX3FPuW7BV9Ljbw7/view?usp=sharing"
+            },
+            {
+                titol: "Procedimiento cosedora",
+                link: "https://drive.google.com/file/d/1DtKfWFbhu5kbjZJiiCvdmJ1QG-M-Nffv/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "CTP",
+        pacs: [
+            {
+                titol: "Manteniment",
+                link: "https://drive.google.com/file/d/1TjFIT-ub4FMmEbA6qSSi3rCqiC3bb5va/view?usp=sharing"
+            },
+            {
+                titol: "Creación de planchas CTP",
+                link: "https://drive.google.com/file/d/1g3ed0oICCODTteB8K2d_-mm5ziOPacfr/view?usp=sharing"
+            }
+        ]
+    }, // finish 1
+    {
+        nom: "Fitxes de seguretat Offset",
+        pacs: [
+            {
+                titol: "Fitxes de seguretat Offset",
+                link: "https://drive.google.com/file/d/1aTXDWIqynyQvAi1wquK1pJNqcLKJqSuC/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Grapadora",
+        pacs: [
+            {
+                titol: "Nagel Rinak",
+                link: "https://drive.google.com/file/d/18QostifqpDrOBa6B7DPvMw6OyDLXvqR8/view?usp=sharing"
+            },
+            {
+                titol: "Procediment grapadora",
+                link: "https://drive.google.com/file/d/16lXUroLGsXHy8h-QrIIa-B3IhcCRm7vC/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "GTO",
+        pacs: [
+            {
+                titol: "Ficha procedimiento",
+                link: "https://drive.google.com/file/d/1bSgy00vgCpUdNULEy-PANqz7PIMFeJHO/view?usp=sharing"
+            },{
+                titol: "Ficha de seguridad",
+                link: "https://drive.google.com/file/d/1_gwV_Je8-9fsReQ22E2ixIGHkKy3bwXX/view?usp=sharing"
+            },{
+                titol: "Manteniment",
+                link: "https://drive.google.com/file/d/1bSgy00vgCpUdNULEy-PANqz7PIMFeJHO/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "Guillotina",
+        pacs: [
+            {
+                titol: "Ficha de seguridad",
+                link: "https://drive.google.com/file/d/11vmIHXggtJXkyrtW_FLnceh4c6oitmXO/view?usp=sharing"
+            },
+            {
+                titol: "Manteniment guillotina",
+                link: "https://drive.google.com/file/d/1e7nBXrYRiBySNNmezCawAx8ozxgeG4ig/view?usp=sharing"
+            },
+            {
+                titol: "Procediment guillotina",
+                link: "https://drive.google.com/file/d/1B3kuEnKohHy65zUnzmJVox0d_x-RNtt0/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "IGT",
+        pacs: [
+            {
+                titol: "Mostres d'impresió",
+                link: "https://drive.google.com/file/d/1yfYY2BwlopWK_l9n2QzFIYUvKjJsoMq3/view?usp=sharing"
+            }
+        ]
+    }, // finish 2
+    {
+        nom: "Insoladora",
+        pacs: [
+            {
+                titol: "Insoladora",
+                link: "https://drive.google.com/file/d/16nQgPhRm6csky7fquJW4sMC4weKFLzak/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Inventari màquinas taller Òfset",
+        pacs: [
+            {
+                titol: "Inventari",
+                link: "https://drive.google.com/file/d/1wJCfoyMKMUEcVyqNtd5YIWfsunl8Y1hV/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Laminadora",
+        pacs: [
+            {
+                titol: "Laminadora manual LMM130",
+                link: "https://drive.google.com/file/d/1DoDgIusZZ4V6XCTXcFTx7nZZ7-wkNNRT/view?usp=sharing"
+            },
+            {
+                titol: "Procediment laminadora",
+                link: "https://drive.google.com/file/d/1qsICLd_Je4d7ppHNhZtbW-rvO7jTlsQr/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "Màquina tampogràfica",
+        pacs: [
+            {
+                titol: "Manteniment",
+                link: "https://drive.google.com/file/d/1f1Fsl1T3QGG9LD0BD7wds8oaFVQX3TSN/view?usp=sharing"
+            },
+            {
+                titol: "Mugen Seiki MPC-2PS90",
+                link: "https://drive.google.com/file/d/19S_KSI5BhlatZ-FfzOZiB6s8X_qTQ4wD/view?usp=sharing"
+            }
+            ,
+            {
+                titol: "Procedimients tampografía",
+                link: "https://drive.google.com/file/d/1r5xAQFmvezBc2x3QHJFhCVyAXzTHqAXW/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "Ploter de Tall",
+        pacs: [
+            {
+                titol: "Ficha de seguridad",
+                link: "https://drive.google.com/file/d/1sDkOLrG8s6UXHuD-eVeg5cOxwYcH7EJv/view?usp=sharing"
+            },
+            {
+                titol: "Ploter de tall",
+                link: "https://drive.google.com/file/d/11PW2RHmlv-oGM8zk1KUXeynG9nPo2mUb/view?usp=sharing"
+            }
+        ]
+    }, // finish 3
+    {
+        nom: "Premsa d'estampació",
+        pacs: [
+            {
+                titol: "Procediment premsa",
+                link: "https://drive.google.com/file/d/1xaRYSM-F9AC7ZOoknvIp81r0THQTi1hJ/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Secadora",
+        pacs: [
+            {
+                titol: "TMS KEIFLASH SERIGRAFIA",
+                link: "https://drive.google.com/file/d/1REU0SHv3stE2gEAoWYbqevgvslAi73_Y/view?usp=sharing"
+            }
+        ]
+    },
+    {
+        nom: "Serigrafía",
+        pacs: [
+            {
+                titol: "Procediment serigrafía",
+                link: "https://drive.google.com/file/d/1LUv6L9D_phKbn9yfl_EjJbO-OEidrwhm/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "SpectroDens",
+        pacs: [
+            {
+                titol: "Procediment SpectroDens",
+                link: "https://drive.google.com/file/d/1tMcy6-4qRJy3Twae20CD6FuqJzziex25/view?usp=sharing"
+            }
+        ]
+    },{
+        nom: "Speed Master",
+        pacs: [
+            {
+                titol: "Canvi de planxa SM",
+                link: "https://drive.google.com/file/d/17tSAdaq0ooM3A7MmNak6iebYbndp6BGc/view?usp=sharing"
+            },
+            {
+                titol: "Ficha de seguridad",
+                link: "https://drive.google.com/file/d/1NRL3s0X-xHWj7Aq5jVaBwDIdyTFBWafi/view?usp=sharing"
+            },
+            {
+                titol: "Heidelberg Speed Master",
+                link: "https://drive.google.com/file/d/1uocOWUa_tKELl0TllJxHQD_V1xrPivix/view?usp=sharing"
+            },
+            {
+                titol: "Pass paper SM",
+                link: "https://drive.google.com/file/d/1N-LBP7LMj_5GvrFOh8vxZs_sWplEPzyc/view?usp=sharing"
+            },
+            {
+                titol: "Speed Master",
+                link: "https://drive.google.com/file/d/1KCux-9_-R7Qer-IkMGYDz_yMPyyroGt7/view?usp=sharing"
+            }
+        ]
     }
+];
 
-    get theHtml(){
-        return this.html();
-    }
-    
-    html(){
-             return `<div class="maquina">
-                    <h3>${this.maquina}</h3>
-                    <a href="${this.procediment}" 
-                        target="_blank">Fitxa de procediment</a>
-                    <a href="${this.seguretat}" 
-                        target="_blank">Fitxa de seguretat</a>
-                    <a href= ${this.manteniment}
-                        target="_blank">Manteniment</a>
-                </div>`;
+var main = document.getElementById("main");
+
+for(let i = 0; i<assignatures.length; i++){
+    main.innerHTML += `<section class="square">
+                            <h3>${assignatures[i].nom}</h3>
+                        </section>`
+}
+
+var classes = document.getElementsByClassName("square");
+
+for(let j = 0; j<assignatures.length; j++){
+    for(let i = 0; i<assignatures[j].pacs.length; i++){
+        classes[j].innerHTML += `<a href="${assignatures[j].pacs[i].link}" target="_#blank"><p>${assignatures[j].pacs[i].titol}</p></a>`
     }
 }
 
-var GTO = new maquines("CTO", 
-                        "https://drive.google.com/file/d/1bSgy00vgCpUdNULEy-PANqz7PIMFeJHO/view?usp=sharing",
-                        "https://drive.google.com/file/d/1_gwV_Je8-9fsReQ22E2ixIGHkKy3bwXX/view?usp=sharing",
-                        "https://drive.google.com/file/d/1Ni5gCDNJyftCqqvow0Z4aGCaONjd413c/view?usp=sharing");
 
-var Guillotina = new maquines("Guillotina", 
-                        "https://drive.google.com/file/d/11vmIHXggtJXkyrtW_FLnceh4c6oitmXO/view?usp=sharing",
-                        "https://drive.google.com/file/d/1dGJrz6F2Mpmmbug3ZflWrmVkn0Dw0MHz/view?usp=sharing",
-                        "https://drive.google.com/file/d/1e7nBXrYRiBySNNmezCawAx8ozxgeG4ig/view?usp=sharing");
-
-var Plotter = new maquines("Plotter de tall", 
-                        "https://drive.google.com/file/d/11PW2RHmlv-oGM8zk1KUXeynG9nPo2mUb/view?usp=sharing",
-                        "https://drive.google.com/file/d/1sDkOLrG8s6UXHuD-eVeg5cOxwYcH7EJv/view?usp=sharing",
-                        "https://drive.google.com/file/d/1e7nBXrYRiBySNNmezCawAx8ozxgeG4ig/view?usp=sharing");
-
-var Speedmaster = new maquines("Speedmaster", 
-                        "https://drive.google.com/file/d/1NRL3s0X-xHWj7Aq5jVaBwDIdyTFBWafi/view?usp=sharing",
-                        "https://drive.google.com/file/d/1uocOWUa_tKELl0TllJxHQD_V1xrPivix/view?usp=sharing",
-                        "https://drive.google.com/file/d/1KCux-9_-R7Qer-IkMGYDz_yMPyyroGt7/view?usp=sharing");
-
-var content = document.getElementById("content");
-var allMachines = [GTO.theHtml, Guillotina.theHtml, Plotter.theHtml, Speedmaster.theHtml]
-
-
-for(let i = 0; i<allMachines.length; i++){
-    content.innerHTML += allMachines[i];
-}
 
 
